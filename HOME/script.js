c2151cc0f3397931fc7039a2c08a707347609f83
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-08-02 16:27:38 -0300

    sections 02/03 refeitas, update section04, criado footer

--- HOME/script.js
+++ HOME/script.js
@@ -5,10 +5,13 @@ function conferirMensagemWhatsApp()
 {
     var nome = document.getElementById("nome").value
     var mensagem = document.getElementById("mensagem").value
+    var confDiv = document.getElementById("contact_right")
+    console.log(confDiv)
 
     nomeGlobal = nome
     msgGlobal = mensagem
 
+    confDiv.style.display = "flex"
     document.getElementById("confNome").textContent = nome
     document.getElementById("confMsg").textContent = mensagem
 }
